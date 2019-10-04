d3f6a7bd050965f4b79a24a657223d97537f3ab9
Alexander <aozertsov@gmail.com>
2019-10-04 22:00:25 +0300

    send attachments

--- TelegramConnector.py
+++ TelegramConnector.py
@@ -2,6 +2,7 @@ import telegram
 from telegram.utils.request import Request
 import requests
 import config
+from io import BytesIO
 
 
 class TelegramConnector:
@@ -31,9 +32,10 @@ class TelegramConnector:
         link = config.TELEGRAM_PROXY_LIST_URL
         return requests.get(link).text.split('\n')
 
-    # TODO send attachemnts
     def send_message(self, message):
-        self._send_message(message.body)
+        self._send_message(str(message.body))
+        for attachment in message.attachments:
+            self._send_attachment(attachment)
 
     def _send_message(self, text):
         if not self._is_valid_connection():
@@ -41,9 +43,9 @@ class TelegramConnector:
 
         self.bot.send_message(config.CHAT_ID, text)
 
-    # TODO pass binary stream
-    def _send_attachment(self, file_path):
+    def _send_attachment(self, file):
         if not self._is_valid_connection():
             self._auth()
 
-        self.bot.send_document(config.CHAT_ID, open(file_path, 'rb'))
+        wrapped_data = BytesIO(file.data)
+        self.bot.send_document(config.CHAT_ID, document = wrapped_data, filename= file.name)
